44a806f746b8164fa41a0fd24f0f45d35f982277
UroobaEjaz <Urooba.Ejaz@edu.sait.ca>
2024-01-16 14:57:00 -0700

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+// student-info.js
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <p>Name: UroobaEjaz</p>
+      <Link href="https://github.com/UroobaEjaz/cprg306-assignments">
+      https://github.com/UroobaEjaz/cprg306-assignments
+      </Link>
+    </div>
+  );
+};
+
+export default StudentInfo;

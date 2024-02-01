5959f0184b24edcba04881f1451016343c08b4d8
UroobaEjaz <Urooba.Ejaz@edu.sait.ca>
2024-02-01 13:02:04 -0700

    changes

--- app/page.js
+++ app/page.js
@@ -1,11 +1,32 @@
 import Image from 'next/image'
 import Page from "./week-2/page"
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-     <Page/>
+    <main className="mb-4 p-3 my-4 bg-black text-white min-h-screen ">
+      <h1 className="mb-4 text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
+      <ul className="list-none">
+        <li className="mb-2 p-1 text-xl ">
+          <Link href='./week-2'>Week 2</Link>
+        </li>
+        <li className="mb-2 p-1 text-xl">
+          <Link href='./week-3'>Week 3</Link>
+        </li>
+        <li className="mb-2 p-1 text-xl">
+          week 4
+        </li>
+        <li className="mb-2 p-1 text-xl">
+          week 5
+        </li>
+        <li className="mb-2 p-1 text-xl">
+          week 6
+        </li>
+        <li className="mb-2 p-1 text-xl">
+          week 7
+        </li>
+      </ul>
+      
       
     </main>
   )

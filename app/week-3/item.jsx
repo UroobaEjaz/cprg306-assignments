52191af3607a75f5d5d00e3cdb223b70016615e2
UroobaEjaz <Urooba.Ejaz@edu.sait.ca>
2024-02-01 13:21:11 -0700

    changes

--- app/week-3/item.jsx
+++ app/week-3/item.jsx
@@ -4,9 +4,10 @@ import ItemList from "./item-list";
 
 const Item = () => {
   return (
+    <div className='w-full'>
     <ul className="list-none">
       {ItemList.list.map((item, index) => (
-        <li key={index} className="p-2 m-4 bg-slate-900 max-w-sm">
+        <li key={index} className="p-2 m-4 bg-slate-900 max-w-1/2 ">
           <h2 className="text-xl font-bold">{`${item.name}`}</h2>
           <div className="text-sm">
             Buy { `${item.quantity}`} in {`${item.category}`}
@@ -14,6 +15,7 @@ const Item = () => {
         </li>
       ))}
     </ul>
+    </div>
   );
 };
 
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -11,7 +11,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="bg-gray-950 text-white min-h-screen flex items-center justify-left m-0">
-      <div className="text-left">
+      <div className="text-left w-[400px]">
         <h1 className="text-3xl font-bold mb-6 m-5">Shopping List</h1>
         <Link href="#">
           <Item />

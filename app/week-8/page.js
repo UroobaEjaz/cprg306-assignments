8175b93171ae7e00eba1fa531888d2fca43af3c5
UroobaEjaz <Urooba.Ejaz@edu.sait.ca>
2024-03-26 10:10:06 -0600

    removing Error

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,6 +1,4 @@
-
 "use client";
-
 import { useUserAuth } from "./_utils/auth-context";
 import Link from "next/link";
 
@@ -9,24 +7,23 @@ export default function Page() {
 
   return (
     <main className="flex justify-center items-center h-screen">
-        <div className=" bg-eggshell text-black p-8 rounded-lg w-1/4  flex-rows text-center">
-            <h1 className="mb-10 text-xl font-semibold">Week 8 Assignment</h1>
-            <p className="m-2 text-lg font-semibold">{user ? "Hi there!" : "Please sign in:"}</p>
-            <p className="m-2 text-lg font-semibold">{user && user.displayName}</p>
-            <p className="m-2 text-lg font-semibold">{user ? "Email: "+user.email : ""}</p>
-            <p className="m-2 text-lg font-semibold">
-                {user ? (
-                    <div>
-                    <Link className="underline" href="week-8\shopping-list">Shopping List</Link>
-                    <p><button className="text-lg text-white bg-black rounded-lg p-2 mt-4 cursor:pointer" onClick={firebaseSignOut}>Sign Out</button></p>
-                    </div>
-                ) : (
-                    <div className="">
-                <button className="text-lg text-white bg-black rounded-lg p-2 cursor:pointer" onClick={gitHubSignIn}>Sign In with GitHub</button>
-                </div>
-                )}
-            </p>
+      <div className="bg-eggshell text-black p-8 rounded-lg w-1/4 flex-rows text-center">
+        <h1 className="mb-10 text-xl font-semibold">Week 8 Assignment</h1>
+        <div className="m-2 text-lg font-semibold">
+          {user ? (
+            <div>
+                <p>
+              <Link className="underline" href="week-8\shopping-list">Shopping List</Link>
+              </p>
+              <button className="text-lg text-white bg-black rounded-lg p-2 mt-4 cursor:pointer" onClick={firebaseSignOut}>Sign Out</button>
+            </div>
+          ) : (
+            <div>
+              <button className="text-lg text-white bg-black rounded-lg p-2 cursor:pointer" onClick={gitHubSignIn}>Sign In with GitHub</button>
+            </div>
+          )}
         </div>
+      </div>
     </main>
   );
 }

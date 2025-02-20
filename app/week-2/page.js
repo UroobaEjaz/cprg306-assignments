00a787ea66878f030327760828102bd94628fcbf
UroobaEjaz <Urooba.Ejaz@edu.sait.ca>
2025-02-20 13:38:31 -0700

    update

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -4,10 +4,11 @@ import Link from 'next/link';
 
 export default function Page() {
     return (
-      <main>
-        <Link href='./week-3'> Shopping list
-        </Link>
+      <main className="mb-4 p-3 my-4 bg-black text-white min-h-screen ">
+        <Link href='./week-3'> Shopping list   
         <StudentInfo/>
+         </Link>
+      
       </main>
     );
   }
